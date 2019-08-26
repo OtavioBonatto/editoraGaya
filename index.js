71d95565e96598a7671b7c0f0e22921bfd338d9b
OtavioBonatto <otaviobonatto92@gmail.com>
2019-08-26 13:03:54 -0300

    porta do server

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'))
 });
 
-app.listen('3000', () => console.log('servidor rodando'));
+app.listen(process.env.PORT, () => console.log('servidor rodando'));
